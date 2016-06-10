069ef97491574fecf90ecbce357579f49e169bec
Lucas Schiffer <schifferl@users.noreply.github.com>
2016-06-09 21:09:27 -0400

    changes to downloadAll.R and loadLibraries.R
    to match schifferl/HPCC repo

--- R/downloadAll.R
+++ R/downloadAll.R
@@ -1,20 +1,28 @@
+# load libraries
+library(devtools)
 library(MultiAssayExperiment)
 library(RTCGAToolbox)
 library(BiocInterfaces)
 library(readr)
 
+# newMAEO variables
 ds <- getFirehoseDatasets()[c(1:5, 7:9, 12:14, 16:31, 33:38)]
 rd <- getFirehoseRunningDates()[1]
 ad <- getFirehoseAnalyzeDates()[1]
 dd <- "./data"
 
+# write header row to csv file for unit tests
+header <- data.frame(cbind("cohort_name", "experiment_name", "experiment_class", "feature_number", "sample_number"))
+write.table(header, file = "MAEOinfo.csv", sep = ",", append = TRUE, row.names = FALSE, col.names = FALSE)
+
+# newMAEO function definition
 newMAEO <- function(ds, rd, ad, dd) {
   if(!dir.exists(dd)) {
     dir.create(dd)
   }
   for(i in ds) {
     cn <- tolower(i)
-    fp <- file.path(dd, paste0(cn, ".Rda"))
+    fp <- file.path(dd, paste0(cn, ".rds"))
     if(file.exists(fp)) {
       load(fp)
     } else {
@@ -38,7 +46,7 @@ newMAEO <- function(ds, rd, ad, dd) {
                             destdir = "./tmp",
                             fileSizeLimit = 500000,
                             getUUIDs = FALSE)
-      save(co, file = fp)
+      saveRDS(co, file = fp, compress = "bzip2")
     }
     pd <- co@Clinical
     rownames(pd) <- gsub("\\.", "-", rownames(pd))
@@ -50,10 +58,19 @@ newMAEO <- function(ds, rd, ad, dd) {
     ExpList <- Elist(dataFull)
     NewElist <- TCGAcleanExpList(ExpList, pd)
     NewMap <- TCGAgenerateMap(NewElist, pd)
-    MAEOname <- paste0(cn, "MAEO")
-    assign(paste0(cn, "MAEO"), MultiAssayExperiment(NewElist, pd, NewMap))
-    save(list = MAEOname, file = file.path("./data", paste0(cn, "MAEO.Rda")))
+    MAEO <- MultiAssayExperiment(NewElist, pd, NewMap)
+    saveRDS(MAEO, file = file.path("./data", paste0(cn, "MAEO.rds"), compress = "bzip2"))
+    
+    # create csv file for unit tests
+    cohort_name <- rep(cn, length(Elist(MAEO)))
+    experiment_name <- names(MAEO)
+    experiment_class <- sapply(Elist(MAEO), class)
+    feature_number <- sapply(Elist(MAEO), dim)[1,]
+    sample_number <- sapply(Elist(MAEO), dim)[2,]
+    MAEOinfo <- data.frame(cbind(cohort_name, experiment_name, experiment_class, feature_number, sample_number))
+    write.table(MAEOinfo, file = "MAEOinfo.csv", sep = ",", append = TRUE, row.names = FALSE, col.names = FALSE)
   }
 }
 
+# call newMAEO function
 newMAEO(ds, rd, ad, dd)
--- R/loadLibraries.R
+++ R/loadLibraries.R
@@ -1,16 +1,8 @@
 install.packages("devtools", repos = "http://cran.r-project.org")
-
 source("https://bioconductor.org/biocLite.R")
 biocLite()
-
-BiocInstaller::biocLite("vjcitn/MultiAssayExperiment", type = "source")
-library(MultiAssayExperiment)
-
+BiocInstaller::useDevel()
+BiocInstaller::biocLite("vjcitn/MultiAssayExperiment")
 BiocInstaller::biocLite("LiNk-NY/RTCGAToolbox")
-library(RTCGAToolbox)
-
 BiocInstaller::biocLite("waldronlab/BiocInterfaces")
-library(BiocInterfaces)
-
 install.packages("readr", repos = "http://cran.r-project.org")
-library(readr)
