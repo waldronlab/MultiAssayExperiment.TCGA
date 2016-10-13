fff8e027edf856d1513fbd5be15f12b30c346042
LiNk-NY <mramos09@gmail.com>
2016-10-13 18:19:29 -0400

    add disease code specific functions

--- data-raw/downloadDropbox.R
+++ data-raw/downloadDropbox.R
@@ -10,7 +10,7 @@ drop_auth()
 
 drop_acc() %>% select(uid, display_name, email_verified, quota_info.quota)
 
-BoxSubTypes <- drop_dir("The Cancer Genome Atlas/Script/allsubtypes")[["path"]]
+BoxSubTypes <- rdrop2::drop_dir("The Cancer Genome Atlas/Script/allsubtypes")[["path"]]
 
 if (!file.exists("./inst/extdata/allsubtypes/")) {
     dir.create("./inst/extdata/allsubtypes/", recursive = TRUE)
@@ -18,11 +18,21 @@ if (!file.exists("./inst/extdata/allsubtypes/")) {
 
 ## Download all subtype files
 invisible(lapply(BoxSubTypes, function(archive) {
-    drop_get(archive, local_file = file.path("./inst/extdata/allsubtypes/",
-                                             basename(archive)),
+    drop_get(archive, local_file = file.path(subtypePath, basename(archive)),
              overwrite = TRUE)
 }))
 
+getSubtypeFile <- function(diseaseCode, overwrite=TRUE) {
+    invisible(BoxSubTypes <-
+        rdrop2::drop_dir("The Cancer Genome Atlas/Script/allsubtypes")[["path"]])
+    subtypePath <- file.path(".", "inst", "extdata", "allsubtypes")
+    subtypeFile <- BoxSubTypes[grepl(paste0(diseaseCode, ".csv"),
+                                     basename(BoxSubTypes), fixed = TRUE)]
+    if (rdrop2::drop_get(subtypeFile, local_file =
+                         file.path(subtypePath, basename(subtypeFile)),
+                     overwrite = overwrite))
+        message("download successful")
+}
 
 ## Download all curated clinical files
 BoxClinicalCuration <- drop_dir("The Cancer Genome Atlas/TCGA_Clinical_Curation")[["path"]]
@@ -36,6 +46,17 @@ invisible(lapply(BoxClinicalCuration, function(archive) {
                                              basename(archive)),
              overwrite = TRUE)
 }))
+
+getClinicalFile <- function(diseaseCode, overwrite=TRUE) {
+    invisible(BoxClinicalCuration <-
+        drop_dir("The Cancer Genome Atlas/TCGA_Clinical_Curation")[["path"]])
+    clinicalPath <- file.path(".", "inst", "extdata", "TCGA_Curation_Cancer_Types")
+    clinicalFile <- BoxClinicalCuration[grepl(paste0("TCGA_Variable_Curation_", diseaseCode, ".xlsx"),
+                                              basename(BoxClinicalCuration))]
+    if (rdrop2::drop_get(clinicalFile, local_file = file.path(clinicalPath, basename(clinicalFile)),
+                         overwrite = overwrite))
+        message("download successful")
+}
 
 ## Run brcaMerge to merge BRCA2 to BRCA and remove BRCA2
 source("data-raw/brcaMerge.R")
