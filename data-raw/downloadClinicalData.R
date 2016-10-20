67c53dabcffe9fd857c06a42bf2f2e10270d557d
LiNk-NY <mramos09@gmail.com>
2016-10-19 20:36:10 -0400

    add new folder structure and functions for individual download

--- data-raw/downloadClinicalData.R
+++ data-raw/downloadClinicalData.R
@@ -5,36 +5,38 @@ library(BiocInterfaces)
 
 rD <- getFirehoseRunningDates(last = 1)
 ## 20151101
-
-if (!file.exists("./inst/extdata/Clinical")) {
-    dir.create("./inst/extdata/Clinical/", recursive = TRUE)
-}
-
 excludeDatasets <- c("COADREAD", "GBMLGG", "KIPAN", "STES", "FPPP")
 diseases <- getFirehoseDatasets()[!(getFirehoseDatasets() %in% excludeDatasets)]
 
-TCGAclin <- lapply(diseases,
-                   function(dx) {
-                       getFirehoseData(dx,
-                                       runDate="20151101",
-                                       Clinic = TRUE,
-                                       destdir = "./inst/extdata/")
-                   })
-names(TCGAclin) <- diseases
+extData <- "inst/extdata"
+clinicalData <- file.path(extData, "Clinical")
+rawClinical <- file.path(clinicalData, "raw")
+basicClinical <- file.path(clinicalData, "basic")
 
-full_TCGAclin <- lapply(TCGAclin, function(dx) { dx@Clinical })
+invisible(lapply(list(clinicalData, rawClinical, basicClinical), function(folder) {
+    if (!file.exists(folder))
+        dir.create(folder, recursive = TRUE)
+}))
 
-## prevent loss of rownames in data
-full_TCGAclin <- lapply(full_TCGAclin,
-                        function(cl) {
-                            cbind(patientID = rownames(cl), cl)
-                        })
+getClinicalFirehose <- function(diseaseCode, force = FALSE) {
+    runDate <- "20151101"
+    extData <- "inst/extdata"
+    clinicalData <- file.path(extData, "Clinical")
+    rawClinical <- file.path(clinicalData, "raw")
+    ClinicalFileName <- paste0(runDate, "-", diseaseCode, "-", "Clinical.txt")
+    if (file.exists(file.path(rawClinical, ClinicalFileName)) & !force) {
+        message(sQuote(ClinicalFileName), " already exists")
+    } else {
+        TCGAclin <- getFirehoseData(diseaseCode, runDate=runDate, Clinic = TRUE,
+                                    destdir = rawClinical)
+    }
+}
 
-lapply(seq_along(full_TCGAclin),
-       function(dx, i) {
-           write_csv(dx[[i]],
-                     path = file.path("./inst/extdata/Clinical/",
-                                      paste0(diseases[i], ".csv")))},
-       dx = full_TCGAclin)
-
-clin_files <- dir("./inst/extdata/Clinical/", full.names = TRUE)
+saveClinicalFirehose <- function(diseaseCode) {
+    runDate <- "20151101"
+    TCGAclin <- getFirehoseData(diseaseCode, runDate = runDate, Clinic = TRUE,
+                                destdir = rawClinical)
+    full_TCGAclin <- Clinical(TCGAclin)
+    full_TCGAclin <- cbind(patientID = rownames(full_TCGAclin), full_TCGAclin)
+    write_csv(full_TCGAclin, path=file.path(basicClinical, paste0(diseaseCode, ".csv")))
+}
