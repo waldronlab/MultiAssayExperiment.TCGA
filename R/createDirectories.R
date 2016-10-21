896718cc29491e88b9f09a9a5f7e754e2968af3b
LiNk-NY <mramos09@gmail.com>
2016-10-20 23:51:29 -0400

    edit directories

--- R/createDirectories.R
+++ R/createDirectories.R
@@ -1,16 +1,21 @@
 createDirectories <- function() {
+        extData <- "inst/extdata/"
+        clinicalData <- file.path(extData, "Clinical")
     directories <- list(
-        extData = "inst/extdata/",
         curatedSubtypes = file.path(extData, "curatedSubtypes"),
         subtypePath = file.path(extData, "allsubtypes"),
-        clinicalData = file.path(extData, "Clinical"),
         clinicalCurationPath = file.path(extData, "TCGA_Clinical_Curation"),
         rawClinical = file.path(clinicalData, "raw"),
         basicClinical = file.path(clinicalData, "basic")
     )
-    invisible(lapply(list(directories), function(folder) {
-        if (!file.exists(folder))
-            dir.create(folder, recursive = TRUE)
-    }))
+    directories <- c(extData = extData,
+                       clinicalData = clinicalData,
+                       directories)
+    invisible(lapply(directories,
+                     function(folder) {
+                         if (!file.exists(folder))
+                             dir.create(folder, recursive = TRUE)
+                     }))
     return(directories)
 }
+
