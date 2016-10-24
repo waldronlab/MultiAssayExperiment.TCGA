3df2501b730efe36ddfa55d401d6f2c493ef1411
LiNk-NY <mramos09@gmail.com>
2016-10-24 19:13:34 -0400

    Fix GBM dataset, slice misspecified

--- data-raw/ExcelPreprocess/excel_GBM_preproc.R
+++ data-raw/ExcelPreprocess/excel_GBM_preproc.R
@@ -5,10 +5,18 @@ library(dplyr)
 library(readr)
 
 gbmxl <- read_excel("data-raw/ExcelPreprocess/mmc2_GBM.xlsx", skip = 1)
+gbmxl <- split(gbmxl, gbmxl$Study)
 
-lggdata <- dplyr::slice(gbmxl, Study == "Brain Lower Grade Glioma" )
+lggdata <- gbmxl[["Brain Lower Grade Glioma"]]
 
-gbmdata <- dplyr::slice(gbmxl, Study == "Glioblastoma multiforme")
+gbmdata <- gbmxl[["Glioblastoma multiforme"]]
 
-write_csv(lggdata, file = "data-raw/ExcelPreprocess/LGG.csv")
-write_csv(gbmdata, file = "data-raw/ExcelPreprocess/GBM.csv")
+stopifnot(nrow(gbmxl) == sum(nrow(lggdata), nrow(gbmdata)))
+
+write_csv(lggdata, path = "data-raw/ExcelPreprocess/LGG.csv")
+
+write_csv(gbmdata, path = "data-raw/ExcelPreprocess/GBM.csv")
+
+rdrop2::drop_upload("data-raw/ExcelPreprocess/GBM.csv",
+                    dest = "The Cancer Genome Atlas/Script/allsubtypes/",
+                    overwrite = TRUE)
