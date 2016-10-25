dc63212bc4c4970f6f5907061ebbe6d83fcda0b2
LiNk-NY <mramos09@gmail.com>
2016-10-25 14:16:14 -0400

    add argument to optionally build dirs

--- R/createDirectories.R
+++ R/createDirectories.R
@@ -1,21 +1,24 @@
-createDirectories <- function() {
-        extData <- "inst/extdata/"
-        clinicalData <- file.path(extData, "Clinical")
-    directories <- list(
-        curatedSubtypes = file.path(extData, "curatedSubtypes"),
-        subtypePath = file.path(extData, "allsubtypes"),
-        clinicalCurationPath = file.path(extData, "TCGA_Clinical_Curation"),
-        rawClinical = file.path(clinicalData, "raw"),
-        basicClinical = file.path(clinicalData, "basic")
+dataDirectories <- function(create=FALSE) {
+    extData <- "inst/extdata/"
+    clinicalData <- file.path(extData, "Clinical")
+    directories <- list(extData = extData,
+                        clinicalData = clinicalData,
+                        curatedSubtypes =
+                            file.path(extData, "curatedSubtypes"),
+                        curatedMaps = file.path(extData, "curatedSubtypes",
+                                                "curatedMaps"),
+                        subtypePath = file.path(extData, "allsubtypes"),
+                        clinicalCurationPath =
+                            file.path(extData, "TCGA_Clinical_Curation"),
+                        rawClinical = file.path(clinicalData, "raw"),
+                        basicClinical = file.path(clinicalData, "basic")
     )
-    directories <- c(extData = extData,
-                       clinicalData = clinicalData,
-                       directories)
-    invisible(lapply(directories,
-                     function(folder) {
-                         if (!file.exists(folder))
-                             dir.create(folder, recursive = TRUE)
-                     }))
+    if (create) {
+        invisible(lapply(directories,
+                         function(folder) {
+                             if (!file.exists(folder))
+                                 dir.create(folder, recursive = TRUE)
+                         }))
+    }
     return(directories)
 }
-
