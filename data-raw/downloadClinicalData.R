9fe487be797eada03a1263c1b4a961f79d000801
LiNk-NY <mramos09@gmail.com>
2016-10-28 16:53:27 -0400

    edit paths in clin dL

--- data-raw/downloadClinicalData.R
+++ data-raw/downloadClinicalData.R
@@ -8,35 +8,30 @@ rD <- getFirehoseRunningDates(last = 1)
 excludeDatasets <- c("COADREAD", "GBMLGG", "KIPAN", "STES", "FPPP")
 diseases <- getFirehoseDatasets()[!(getFirehoseDatasets() %in% excludeDatasets)]
 
-extData <- "inst/extdata"
-clinicalData <- file.path(extData, "Clinical")
-rawClinical <- file.path(clinicalData, "raw")
-basicClinical <- file.path(clinicalData, "basic")
-
-invisible(lapply(list(clinicalData, rawClinical, basicClinical), function(folder) {
-    if (!file.exists(folder))
-        dir.create(folder, recursive = TRUE)
-}))
 
 getClinicalFirehose <- function(diseaseCode, force = FALSE) {
     runDate <- "20151101"
-    extData <- "inst/extdata"
-    clinicalData <- file.path(extData, "Clinical")
-    rawClinical <- file.path(clinicalData, "raw")
+    dirList <- dataDirectories()
     ClinicalFileName <- paste0(runDate, "-", diseaseCode, "-", "Clinical.txt")
-    if (file.exists(file.path(rawClinical, ClinicalFileName)) & !force) {
+    if (file.exists(file.path(dirList[["rawClinical"]], ClinicalFileName)) &
+        !force) {
         message(sQuote(ClinicalFileName), " already exists")
     } else {
-        TCGAclin <- getFirehoseData(diseaseCode, runDate=runDate, Clinic = TRUE,
-                                    destdir = rawClinical)
+        TCGAclin <- RTCGAToolbox::getFirehoseData(diseaseCode, runDate=runDate,
+                                                  Clinic = TRUE,
+                                                  destdir =
+                                                      dirList[["rawClinical"]])
     }
 }
 
 saveClinicalFirehose <- function(diseaseCode) {
     runDate <- "20151101"
-    TCGAclin <- getFirehoseData(diseaseCode, runDate = runDate, Clinic = TRUE,
-                                destdir = rawClinical)
+    dirList <- dataDirectories()
+    TCGAclin <- RTCGAToolbox::getFirehoseData(diseaseCode, runDate = runDate, Clinic = TRUE,
+                                              destdir = dirList[["rawClinical"]])
     full_TCGAclin <- TCGAclin@Clinical
-    full_TCGAclin <- cbind(patientID = rownames(full_TCGAclin), full_TCGAclin)
-    write_csv(full_TCGAclin, path=file.path(basicClinical, paste0(diseaseCode, ".csv")))
+    stdBarcodes <- .stdIDs(rownames(full_TCGAclin))
+    full_TCGAclin <- cbind(patientID = stdBarcodes, full_TCGAclin)
+    readr::write_csv(full_TCGAclin, path=file.path(dirList[["basicClinical"]],
+                                                   paste0(diseaseCode, ".csv")))
 }
