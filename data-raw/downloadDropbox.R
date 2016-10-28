ff88dca2620ed017fc0da1cc7a92ca254961ebe6
LiNk-NY <mramos09@gmail.com>
2016-10-28 16:18:04 -0400

    fix data paths with dataDirectories

--- data-raw/downloadDropbox.R
+++ data-raw/downloadDropbox.R
@@ -14,28 +14,19 @@ BoxSubTypes <- rdrop2::drop_dir("The Cancer Genome Atlas/Script/allsubtypes")[["
 BoxClinicalData <- rdrop2::drop_dir("The Cancer Genome Atlas/Clinical/data")[["path"]]
 BoxClinicalCuration <- rdrop2::drop_dir("The Cancer Genome Atlas/TCGA_Clinical_Curation")[["path"]]
 
-extData <- "./inst/extdata/"
-subtypePath <- "allsubtypes"
-# clinicalDataPath <- "Clinical" ## download using downloadClinicalData.R file
-clinicalCurationPath <- "TCGA_Clinical_Curation"
+source("R/dataDirectories.R")
 
-## Build folders
-invisible(lapply(list(subtypePath, clinicalDataPath, clinicalCurationPath),
-                 function(folder, basefolder) {
-                     if (!file.exists(file.path(basefolder, folder))) {
-                         dir.create(file.path(basefolder, folder),
-                                    recursive = TRUE)
-                     }
-                 }, basefolder = extData))
+dirList <- dataDirectories()
 
 ## Download all subtype files
 invisible(lapply(BoxSubTypes, function(archive) {
-    drop_get(archive, local_file = file.path(subtypePath, basename(archive)),
+    drop_get(archive, local_file = file.path(dirList[["subtypePath"]],
+                                             basename(archive)),
              overwrite = TRUE)
 }))
 
 invisible(lapply(BoxClinicalCuration, function(archive) {
-    drop_get(archive, local_file = file.path(clinicalPath,
+    drop_get(archive, local_file = file.path(dirList[["clinicalCurationPath"]],
                                              basename(archive)),
              overwrite = TRUE)
 }))
@@ -44,7 +35,7 @@ invisible(lapply(BoxClinicalCuration, function(archive) {
 getSubtypeFile <- function(diseaseCode, overwrite=TRUE) {
     invisible(BoxSubTypes <-
         rdrop2::drop_dir("The Cancer Genome Atlas/Script/allsubtypes")[["path"]])
-    subtypePath <- file.path(".", "inst", "extdata", "allsubtypes")
+    subtypePath <- dataDirectories()[["subtypePath"]]
     subtypeFile <- BoxSubTypes[grepl(paste0(diseaseCode, ".csv"),
                                      basename(BoxSubTypes), fixed = TRUE)]
     if (rdrop2::drop_get(subtypeFile, local_file =
@@ -57,17 +48,17 @@ getSubtypeFile <- function(diseaseCode, overwrite=TRUE) {
 getClinicalFile <- function(diseaseCode, overwrite=TRUE) {
     invisible(BoxClinicalCuration <-
         drop_dir("The Cancer Genome Atlas/TCGA_Clinical_Curation")[["path"]])
-    clinicalPath <- file.path(".", "inst", "extdata", "TCGA_Clinical_Curation")
+    clinicalCurationPath <- dataDirectories()[["clinicalCurationPath"]]
     clinicalFile <-
         BoxClinicalCuration[grepl(paste0("TCGA_Variable_Curation_",
                                          diseaseCode, ".xlsx"),
                                   basename(BoxClinicalCuration))]
     if (rdrop2::drop_get(clinicalFile,
-                         local_file = file.path(clinicalPath,
+                         local_file = file.path(clinicalCurationPath,
                                                 basename(clinicalFile)),
                          overwrite = overwrite))
         message("download successful")
 }
 
 ## Run brcaMerge to merge BRCA2 to BRCA and remove BRCA2
-source("data-raw/brcaMerge.R")
+# source("data-raw/brcaMerge.R")
