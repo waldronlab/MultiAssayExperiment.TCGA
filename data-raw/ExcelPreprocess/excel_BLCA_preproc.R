f97be125b70ae5b49de9d7c11a0a65f2fbf41ee0
LiNk-NY <mramos09@gmail.com>
2016-11-02 17:52:46 -0400

    Keep order and prioritize first appearance

--- data-raw/ExcelPreprocess/excel_BLCA_preproc.R
+++ data-raw/ExcelPreprocess/excel_BLCA_preproc.R
@@ -5,27 +5,37 @@ blcaxl <- read_excel("data-raw/ExcelPreprocess/Copy of BLCA_Clinical_Data_Table_
                      sheet = 1L)
 
 ## Duplicate column names
-colsDupIdx <- which(duplicated(sort(names(blcaxl))))
-dupCols <- cbind(first = colsDupIdx, second = colsDupIdx-1)
+dupNames <- names(blcaxl)[duplicated(names(blcaxl))]
+dupIdx <- which(duplicated(names(blcaxl)))
+
+## Ensure only duplicated once
+stopifnot(vapply(dupNames, FUN = function(named) {
+    !(sum(names(blcaxl) %in% named) > 2)
+}, FUN.VALUE = logical(1L)))
+
+sortColsDupIdx <- which(duplicated(sort(names(blcaxl))))
+sortColsDupNames <- sort(names(blcaxl))[duplicated(sort(names(blcaxl)))]
+dupCols <- cbind(first = sortColsDupIdx-1, second = sortColsDupIdx)
 
 listDups <- apply(dupCols, 1, function(x) {
     dff <- blcaxl[,sort(names(blcaxl))]
     dff[x]
 })
 
-sortedNames <- sort(names(blcaxl))
-dupNames <- sortedNames[duplicated(sortedNames)]
-stopifnot(vapply(dupNames, FUN = function(named) {
-    !(sum(names(blcaxl) %in% named) > 2)
-}, FUN.VALUE = logical(1L)))
+names(listDups) <- sortColsDupNames
 
 result <- cbind.data.frame(dupCols, identical =
                                vapply(listDups, FUN = function(dff) {
                                    identical(dff[[1L]], dff[[2L]])
-                               }, FUN.VALUE = logical(1L)))
+                               }, FUN.VALUE = logical(1L)),
+                           row.names = names(listDups))
 
+## Return only rows that have identical data in them
 result <- subset(result, result[["identical"]])
-clearNames <- sortedNames[-result[["second"]]]
+
+## Keep order and variables that appear first (match reversed names)
+clearNames <- rev(rev(names(blcaxl))[-match(rownames(result),
+                                            rev(names(blcaxl)))])
 
 processedBLCA <- blcaxl[, clearNames]
 
