cec6ada756505a1af3d8a337ddf48796c077cb02
LiNk-NY <mramos09@gmail.com>
2016-11-04 19:34:20 -0400

    check KIRP for duplicates, more than 2 for some columns

--- data-raw/ExcelPreprocess/excel_KIRP_preproc.R
+++ data-raw/ExcelPreprocess/excel_KIRP_preproc.R
@@ -4,6 +4,18 @@ library(readxl)
 library(dplyr)
 library(readr)
 
-kirpdata <- read_excel("data-raw/ExcelPreprocess/kirp.xlsx", sheet= "KIRP Compiled Clin. & Mol. Data", skip= 2)
+source("data-raw/cleanDuplicates.R")
+
+kirpdata <- read_excel("data-raw/ExcelPreprocess/nejmoa1505917_appendix_3.xlsx",
+                       sheet= "KIRP Compiled Clin. & Mol. Data", skip= 2,
+                        na = "[Not Applicable]")
+
+anyDuplicated(names(kirpdata))
+
+cleanDuplicates(kirpdata)
 
 write_csv(kirpdata, file = "./data-raw/ExcelPreprocess/KIRP.csv")
+
+rdrop2::drop_upload("data-raw/ExcelPreprocess/BLCA.csv",
+                    dest = "The Cancer Genome Atlas/Script/allsubtypes/",
+                    overwrite = TRUE)
