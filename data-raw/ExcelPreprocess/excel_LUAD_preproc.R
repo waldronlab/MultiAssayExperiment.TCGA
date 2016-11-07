47e3c497b5b75680efbdf4aaeb963affa8e3e8b5
LiNk-NY <mramos09@gmail.com>
2016-11-07 16:52:54 -0500

    clean LUAD original subtype data file

---
+++ data-raw/ExcelPreprocess/excel_LUAD_preproc.R
@@ -0,0 +1,35 @@
+## Preprocessing of LUAD
+library(readxl)
+library(dplyr)
+library(readr)
+
+source("data-raw/cleanDuplicates.R")
+source("data-raw/ExcelPreprocess/excel_position.R")
+source("data-raw/cleanDuplicates.R")
+source("R/dataDirectories.R")
+
+## Badly formatted excel file
+content <- readxl::read_excel("data-raw/ExcelPreprocess/lungCarcinoma_LUAD.xlsx",
+                           sheet = "S_Table 7-Clinical&Molec_Summar")[1:3, ]
+
+luad <- readxl::read_excel("data-raw/ExcelPreprocess/lungCarcinoma_LUAD.xlsx",
+                           sheet = "S_Table 7-Clinical&Molec_Summar", skip = 3,
+                           na = "[Not Available]")
+
+## Excel Cell Ranges (M-AI, AM-BP, BQ-CT)
+mutationRange <- excel_position("M"):excel_position("AI")
+CNARange <- excel_position("AM"):excel_position("BP")
+FCNARange <- excel_position("BQ"):excel_position("CT")
+
+names(luad)[mutationRange] <- paste0("Mutation_", names(luad[mutationRange]))
+names(luad)[CNARange] <- paste0("CNA_", names(luad[CNARange]))
+names(luad)[FCNARange] <- paste0("FCNA_", names(luad[FCNARange]))
+
+luad <- cleanDuplicates(luad)
+
+readr::write_csv(luad, "inst/extdata/allsubtypes/LUAD.csv")
+
+rdrop2::drop_upload(file = file.path(dataDirectories()[["subtypePath"]],
+                                     "LUAD.csv"),
+                    dest = "The Cancer Genome Atlas/Script/allsubtypes/",
+                    overwrite = TRUE)
--- data-raw/ExcelPreprocess/excel_SKCM_preproc.R
+++ data-raw/ExcelPreprocess/excel_SKCM_preproc.R
@@ -4,6 +4,7 @@ library(readxl)
 library(dplyr)
 library(readr)
 
-skcmdata <- read_excel("data-raw/ExcelPreprocess/mmc2_skcm.xlsx", sheet= "Supplemental Table S1D", skip= 1)
+skcmdata <- read_excel("data-raw/ExcelPreprocess/mmc2_skcm.xlsx",
+                       sheet= "Supplemental Table S1D", skip= 1)
 
 write_csv(skcmdata, file = "inst/extdata/allsubtypes/SKCM.csv")
