1b58a0c2481094700f2c83b66e881562e62ba426
LiNk-NY <mramos09@gmail.com>
2016-11-07 14:56:10 -0500

    Rename and include LGG GBM preproc

---
+++ data-raw/ExcelPreprocess/excel_LGGGBM_preproc.R
@@ -0,0 +1,26 @@
+## Preprocessing of GBM
+
+library(readxl)
+library(dplyr)
+library(readr)
+
+gbmxl <- read_excel("data-raw/ExcelPreprocess/mmc2_LGGGBM.xlsx", skip = 1)
+gbmxl <- split(gbmxl, gbmxl$Study)
+
+lggdata <- gbmxl[["Brain Lower Grade Glioma"]]
+
+gbmdata <- gbmxl[["Glioblastoma multiforme"]]
+
+stopifnot(nrow(gbmxl) == sum(nrow(lggdata), nrow(gbmdata)))
+
+write_csv(lggdata, path = "inst/extdata/allsubtypes/LGG.csv")
+
+write_csv(gbmdata, path = "inst/extdata/allsubtypes/GBM.csv")
+
+rdrop2::drop_upload("inst/extdata/allsubtypes/LGG.csv",
+                    dest = "The Cancer Genome Atlas/Script/allsubtypes/",
+                    overwrite = TRUE)
+
+rdrop2::drop_upload("inst/extdata/allsubtypes/GBM.csv",
+                    dest = "The Cancer Genome Atlas/Script/allsubtypes/",
+                    overwrite = TRUE)
