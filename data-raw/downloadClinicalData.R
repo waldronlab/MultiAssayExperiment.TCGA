a222cd9015b2fcc777846a8f4d5971057433de68
LiNk-NY <mramos09@gmail.com>
2016-11-23 21:41:36 -0500

    clean up non-dropbox clinical downloads

--- data-raw/downloadClinicalData.R
+++ data-raw/downloadClinicalData.R
@@ -1,11 +1,9 @@
-# BiocInstaller::biocLite("LiNk-NY/RTCGAToolbox")
-library(RTCGAToolbox)
-library(readr)
-library(BiocInterfaces)
+## Script for downloading clinical data without DropBox
 
-# Load vector of TCGA cancer codes
-source("data-raw/diseaseCodes.R")
+## Load vector of TCGA cancer codes
 source("data-raw/helpers.R")
+source("R/getDiseaseCodes.R")
+TCGAcode <- getDiseaseCodes()
 
 getClinicalFirehose <- function(diseaseCode, force = FALSE) {
     runDate <- "20151101"
@@ -19,8 +17,11 @@ getClinicalFirehose <- function(diseaseCode, force = FALSE) {
                                                   Clinic = TRUE,
                                                   destdir =
                                                       dirList[["rawClinical"]])
+        message(diseaseCode, " download successful")
     }
 }
+
+unlist(sapply(TCGAcode, getClinicalFirehose))
 
 saveClinicalFirehose <- function(diseaseCode) {
     runDate <- "20151101"
@@ -34,3 +35,4 @@ saveClinicalFirehose <- function(diseaseCode) {
                                                    paste0(diseaseCode, ".csv")))
 }
 
+lapply(TCGAcode, saveClinicalFirehose)
--- data-raw/downloadExtraClinical.R
+++ data-raw/downloadExtraClinical.R
@@ -1,14 +1,20 @@
 ## Code to create "enhancedClinical" data
 source("data-raw/helpers.R")
+source("R/getDiseaseCodes.R")
+TCGAcodes <- getDiseaseCodes()
 
 ## Write enhancedData to enhanced folder
-writeClinicalData <- function(diseaseCode) {
-    dataset <- .mergeClinicalData(diseaseCode)
+writeClinicalData <- function(diseaseCode, force=FALSE) {
     enhancedPath <- dataDirectories()[["enhancedClinical"]]
-    if (!file.exists(enhancedPath))
-        dataDirectories(TRUE)
-    write_csv(dataset,
+    fileName <- file.path(enhancedPath, paste0(diseaseCode, ".csv"))
+    if (!file.exists(fileName) || force) {
+    dataset <- .mergeClinicalData(diseaseCode)
+    readr::write_csv(dataset,
               path = file.path(enhancedPath,
                                paste0(diseaseCode, ".csv")))
-
+    message(dataset, " with extra columns created")
+    }
+    message(fileName, " available")
 }
+
+invisible(lapply(TCGAcodes, writeClinicalData))
