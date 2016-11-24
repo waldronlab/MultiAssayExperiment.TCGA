da4ab74d73cb7cacc112074de908eb93f7789178
LiNk-NY <mramos09@gmail.com>
2016-11-23 21:37:31 -0500

    exclude TCGA disease codes, include rdrop2

--- R/getDiseaseCodes.R
+++ R/getDiseaseCodes.R
@@ -1,7 +1,8 @@
 ## Function for getting useable barcodes (no RTCGAToolbox dep)
 getDiseaseCodes <- function() {
     data("diseaseCodes", package = "BiocInterfaces")
-    excludedCodes <- c("COADREAD", "GBMLGG", "KIPAN", "STES", "FPPP", "CNTL")
+    excludedCodes <- c("COADREAD", "GBMLGG", "KIPAN", "STES", "FPPP", "CNTL",
+                       "LCML", "MISC")
     logicalSub <- !diseaseCodes[[1L]] %in% excludedCodes
     unname(unlist(diseaseCodes[logicalSub, 1L]))
 }
