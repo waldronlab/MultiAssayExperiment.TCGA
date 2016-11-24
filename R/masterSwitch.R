170d32084d323ea85958808d5502abc4fc6aa433
LiNk-NY <mramos09@gmail.com>
2016-11-23 21:44:43 -0500

    add master file for downloading and merging clinical/subtype data

---
+++ R/masterSwitch.R
@@ -0,0 +1,17 @@
+# Script to run to build all clinical datasets in several folders
+packs <- c("readxl", "readr", "dplyr", "rdrop2", "RTCGAToolbox", "BiocInterfaces")
+sapply(packs, library, character.only = TRUE)
+
+# Allocate all directories in repository
+source("R/dataDirectories.R")
+dataDirectories(create = TRUE)
+
+## Download all available resources
+source("data-raw/downloadClinicalData.R")
+source("data-raw/downloadExtraClinical.R")
+
+## Download SubType data from DropBox
+source("data-raw/downloadSubtypeDrop.R")
+
+## Merge curated data to clinical data
+source("data-raw/mergeSubtypeCuration.R")
