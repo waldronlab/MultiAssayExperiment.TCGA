606949a484dcc6c297c439774256affa90006d00
LiNk-NY <mramos09@gmail.com>
2016-11-23 21:44:24 -0500

    updated checkSubtypeCuration

--- data-raw/checkSubtypeCuration.R
+++ data-raw/checkSubtypeCuration.R
@@ -1,5 +1,7 @@
 ## Script to check subtype data against the maps
+library(dplyr)
 source("data-raw/helpers.R")
+load("data/curationAvailable.rda")
 
 ## How to figure out which datasets don't have matching columns
 ## List of lists (each inner list has names in dataset and names that were
@@ -16,3 +18,34 @@ checkSubtypeCuration <- function(diseaseCode) {
         print("Subtype check passed")
     }
 }
+
+## Fix barcodes that are inconsistent
+curateBarcodes <- function(diseaseCode) {
+    subtypeData <- .readSubtypeData(diseaseCode)
+    bcode <- .findBarcodeCol(subtypeData)
+    if (length(bcode)) {
+        subtypeData[[bcode]] <- .stdIDs(subtypeData[[bcode]])
+    }
+    subtypeData
+}
+
+## See what TCGA disease codes have corrupt barcodes
+findCorruptBarcodes <- function(diseaseCode) {
+    subtypeData <- .readSubtypeData(diseaseCode)
+    bcode <- .findBarcodeCol(subtypeData)
+    if (!length(bcode)) {
+        message("No barcode column found")
+        return(head(subtypeData, 3))
+    } else {
+        message(diseaseCode, " barcodes OK")
+    }
+    return(dplyr::data_frame())
+}
+
+## Find what curated disease don't pass the check
+bcodeRes <- vapply(curationAvailable, FUN = function(dx) {
+    identical(c(0L, 0L), dim(findCorruptBarcodes(dx)))
+}, FUN.VALUE = logical(1L))
+
+which(!bcodeRes)
+stopifnot(all(bcodeRes))
