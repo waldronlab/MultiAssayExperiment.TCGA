74e0e7e07433a6c8aa18989c533f85096f23065d
LiNk-NY <mramos09@gmail.com>
2016-11-23 21:42:55 -0500

    simplify mergeSubtypeCuration, move functions to checkSubtypeCuration

--- data-raw/mergeSubtypeCuration.R
+++ data-raw/mergeSubtypeCuration.R
@@ -1,41 +1,7 @@
 ### Merging subtype files with clinical data
-library(dplyr)
 
-source("R/dataDirectories.R")
-source("data-raw/checkSubtypeCuration.R")
-source("data-raw/helpers.R")
 ## Load available cancer codes
-source("data-raw/diseaseCodes.R")
-
-curateBarcodes <- function(diseaseCode) {
-    subtypeData <- .readSubtypeData(diseaseCode)
-    bcode <- .findBarcodeCol(subtypeData)
-    if (length(bcode)) {
-        subtypeData[[bcode]] <- .stdIDs(subtypeData[[bcode]])
-    }
-    subtypeData
-}
-
-## See what files have corrupt barcodes
-findCorruptBarcodes <- function(diseaseCode) {
-    subtypeData <- .readSubtypeData(diseaseCode)
-    bcode <- .findBarcodeCol(subtypeData)
-    if (!length(bcode)) {
-        message("No barcode column found")
-        return(head(subtypeData, 3))
-    } else {
-        message(diseaseCode, " barcodes OK")
-    }
-    return(dplyr::data_frame())
-}
-
-
-bcodeRes <- vapply(curationAvailable, FUN = function(dx) {
-    identical(c(0L, 0L), dim(findCorruptBarcodes(dx)))
-}, FUN.VALUE = logical(1L))
-
-which(!bcodeRes)
-stopifnot(all(bcodeRes))
+load("data/curationAvailable.rda")
 
 ## Save final merged datasets
 writeMergedClinical <- function(diseaseCode, curationAvailable) {
@@ -45,6 +11,6 @@ writeMergedClinical <- function(diseaseCode, curationAvailable) {
         path = file.path(mergedLocation, paste0(diseaseCode, "_merged.csv")))
 }
 
-lapply(includeDatasets, writeMergedClinical,
+lapply(TCGAcodes, writeMergedClinical,
        curationAvailable=curationAvailable)
 
