18aeb0c59c2c4a120e7cb2f8741d32d9b7199b5f
LiNk-NY <mramos09@gmail.com>
2016-11-30 18:30:36 -0500

    Revise runPipeline script

--- R/runPipeline.R
+++ R/runPipeline.R
@@ -4,13 +4,15 @@ library(RTCGAToolbox)
 library(BiocInterfaces)
 library(readr)
 
-source("data-raw/diseaseCodes.R")
+source("R/getDiseaseCodes.R")
 source("data-raw/helpers.R")
 
-# newMAEO variables
-TCGAcodes <- includeDatasets
-runDate <- getFirehoseRunningDates(last=1)
-analyzeDate <- getFirehoseAnalyzeDates(last=1)
+# Create MultiAssayExperiments for each TCGA disease code
+TCGAcodes <- getDiseaseCodes()
+# runDate <- getFirehoseRunningDates(last=1)
+runDate <- "20151101"
+# analyzeDate <- getFirehoseAnalyzeDates(last=1)
+analyzeDate <- "20150821"
 dataDirectory <- "data"
 
 # write header row to csv file for unit tests
@@ -22,130 +24,110 @@ if(!file.exists("MAEOinfo.csv")) {
         append = TRUE, row.names = FALSE, col.names = FALSE)
 }
 
-# newMAEO function definition
-newMAEO <- function(TCGAcodes, runDate, analyzeDate, dataDirectory) {
-    if(!dir.exists(dataDirectory)) {
-    dir.create(dataDirectory)
-  }
+# buildMultiAssayExperiments function definition
+buildMultiAssayExperiments <-
+    function(TCGAcodes, runDate, analyzeDate, dataDirectory) {
+        if (!dir.exists(dataDirectory))
+            dir.create(dataDirectory)
 
-  for(cancer in TCGAcodes) {
-    message("\n######\n", "\nProcessing ", cancer, " : )\n", "\n######\n")
-    cancerCode <- toupper(cancer)
-    serialPath <- file.path(dataDirectory, paste0(cancerCode, ".rds"))
+        for (cancer in TCGAcodes) {
+            message("\n######\n", "\nProcessing ", cancer, " : )\n", "\n######\n")
+            cancerCode <- tolower(cancer)
+            serialPath <- file.path(dataDirectory, paste0(cancerCode, ".rds"))
 
-    if(file.exists(serialPath)) {
-        cancerObject <- readRDS(serialPath)
-    } else {
-      cancerObject <- getFirehoseData(cancer, runDate = runDate,
-                            gistic2_Date = analyzeDate,
-                            RNAseq_Gene = TRUE,
-                            Clinic = FALSE,
-                            miRNASeq_Gene = TRUE,
-                            RNAseq2_Gene_Norm = TRUE,
-                            CNA_SNP = TRUE,
-                            CNV_SNP = TRUE,
-                            CNA_Seq = TRUE,
-                            CNA_CGH = TRUE,
-                            Methylation = TRUE,
-                            Mutation = TRUE,
-                            mRNA_Array = TRUE,
-                            miRNA_Array = TRUE,
-                            RPPA_Array = TRUE,
-                            RNAseqNorm = "raw_counts",
-                            RNAseq2Norm = "normalized_count",
-                            forceDownload = FALSE,
-                            destdir = "./tmp",
-                            fileSizeLimit = 500000,
-                            getUUIDs = FALSE)
-      saveRDS(cancerObject, file = serialPath, compress = "bzip2")
+            if (file.exists(serialPath)) {
+                cancerObject <- readRDS(serialPath)
+            } else {
+                cancerObject <- getFirehoseData(cancer, runDate = runDate,
+                                                gistic2_Date = analyzeDate,
+                                                RNAseq_Gene = TRUE,
+                                                Clinic = FALSE,
+                                                miRNASeq_Gene = TRUE,
+                                                RNAseq2_Gene_Norm = TRUE,
+                                                CNA_SNP = TRUE,
+                                                CNV_SNP = TRUE,
+                                                CNA_Seq = TRUE,
+                                                CNA_CGH = TRUE,
+                                                Methylation = TRUE,
+                                                Mutation = TRUE,
+                                                mRNA_Array = TRUE,
+                                                miRNA_Array = TRUE,
+                                                RPPA_Array = TRUE,
+                                                RNAseqNorm = "raw_counts",
+                                                RNAseq2Norm = "normalized_count",
+                                                forceDownload = FALSE,
+                                                destdir = "./tmp",
+                                                fileSizeLimit = 500000,
+                                                getUUIDs = FALSE)
+                saveRDS(cancerObject, file = serialPath, compress = "bzip2")
+            }
+            ## Include curated clinical data
+            clinicalPath <- file.path(dataDirectories()[["mergedClinical"]],
+                                      paste0(cancer, "_reduced.csv"))
+            stopifnot(file.exists(clinicalPath))
+            clinicalData <- read.csv(clinicalPath, header=TRUE, stringsAsFactors=FALSE)
+            rownames(clinicalData) <- clinicalData[["patientID"]]
+            clinicalData <- S4Vectors::DataFrame(clinicalData)
+            metadata(clinicalData)[["droppedColumns"]] <-
+                readRDS(file.path(dataDirectories()[["mergedClinical"]],
+                                  paste0(cancer, "_dropped.rds")))
+
+            targets <- c(slotNames(cancerObject)[c(5:16)], "gistica", "gistict")
+            names(targets) <- targets
+            dataList <- lapply(targets, function(x) {
+                tryCatch({TCGAextract(cancerObject, x)},
+                         error = function(e) {message(x, " does not contain any data!")})
+            })
+            dataFull <- Filter(function(x){class(x)!="NULL"}, dataList)
+            assayNames <- names(dataFull)
+
+            exps <- c("CNASNP", "CNVSNP", "CNASeq", "CNACGH")
+            lapply(exps, function(dataType) {
+                type <- switch(dataType,
+                               CNASNP = "CNA_SNP",
+                               CNVSNP = "CNV_SNP",
+                               CNASeq = "CNA_Seq",
+                               CNACGH = "CNA_CGH")
+                args <- list(cancer, runDate, TRUE)
+                names(args) <- c("disease", "runDate", type)
+                source_file <- do.call(getFileNames, args = args)
+                genome_build <- gsub("(^.+)_(hg[0-9]{2})_(.+$)", "\\2", x = source_file,
+                                     ignore.case = TRUE)
+                dataFull[[dataType]] <- genome_build
+                source_file <- c(source_file = source_file)
+                metadata(dataFull[[dataType]]) <- c(metadata(dataFull[[dataType]]),
+                                                    source_file)
+                message(dataType, " metadata added")
+            })
+
+            ExpList <- ExperimentList(dataFull)
+            NewElist <- TCGAcleanExpList(ExpList, clinicalData)
+            NewMap <- generateMap(NewElist, clinicalData, TCGAbarcode)
+            MAEO <- MultiAssayExperiment(NewElist, clinicalData, NewMap)
+
+            MAEOmeta <- c(cancer, runDate, analyzeDate, sessionInfo())
+            names(MAEOmeta)[1:3] <- c("cohort_name", "running_date", "analysis_data")
+            metadata(MAEO) <- c(metadata(MAEO), MAEOmeta)
+
+            saveRDS(MAEO, file = file.path(dataDirectory,
+                                           paste0(cancerCode, "MAEO.rds")),
+                    compress = "bzip2")
+
+            # add lines to csv file for unit tests
+            cohort_name <- rep(cancerCode, length(experiments(MAEO)))
+            experiment_names <- names(MAEO)
+            experiment_classes <- vapply(experiments(MAEO), class, character(1L))
+            feature_numbers <- vapply(experiments(MAEO), function(exp) dim(exp)[[1L]],
+                                     integer(1L))
+            sample_numbers <- vapply(experiments(MAEO), function(exp) dim(exp)[[2L]],
+                                    integer(1L))
+            MAEOinfo <- cbind.data.frame(cohort_name, experiment_names, experiment_classes,
+                                         feature_numbers, sample_numbers)
+            write.table(MAEOinfo, file = "MAEOinfo.csv", sep = ",", append = TRUE,
+                        row.names = FALSE, col.names = FALSE)
+        }
     }
-    ## Include curated clinical data
-    clinicalPath <- file.path(dataDirectories()[["mergedClinical"]], paste0(cancer, "_reduced.csv"))
-    stopifnot(file.exists(clinicalPath))
-    clinicalData <- read.csv(clinicalPath, header=TRUE, stringsAsFactors=FALSE)
-    rownames(clinicalData) <- clinicalData[["patientID"]]
-    clinicalData <- S4Vectors::DataFrame(clinicalData)
-    metadata(clinicalData)[["droppedColumns"]] <-
-        readRDS(file.path(dataDirectories()[["mergedClinical"]],
-                          paste0(cancer, "_dropped.rds")))
 
-    targets <- c(slotNames(cancerObject)[c(5:16)], "gistica", "gistict")
-    names(targets) <- targets
-    dataList <- lapply(targets, function(x) {
-                           tryCatch({TCGAextract(cancerObject, x)},
-                               error = function(e) {message(x, " does not contain any data!")})
-                            })
-    dataFull <- Filter(function(x){class(x)!="NULL"}, dataList)
-    assayNames <- names(dataFull)
-
-    if("CNASNP" %in% assayNames) {
-        source_file <- getFileNames(cancer, runDate, CNA_SNP = TRUE)
-        genome_build <- gsub("(^.+)_(hg[0-9]{2})_(.+$)", "\\2", x = source_file,
-                             ignore.case = TRUE)
-        genome(dataFull$CNASNP) <- genome_build
-        source_file <- as.list(source_file)
-        names(source_file) <- "source_file"
-        metadata(dataFull$CNASNP) <- c(metadata(dataFull$CNASNP), source_file)
-    }
-
-    if("CNVSNP" %in% assayNames) {
-        source_file <- getFileNames(cancer, runDate, CNV_SNP = TRUE)
-        genome_build <- gsub("(^.+)_(hg[0-9]{2})_(.+$)", "\\2", x = source_file,
-                             ignore.case = TRUE)
-        genome(dataFull$CNVSNP) <- genome_build
-        source_file <- as.list(source_file)
-        names(source_file) <- "source_file"
-        metadata(dataFull$CNVSNP) <- c(metadata(dataFull$CNVSNP), source_file)
-    }
-
-    if("CNASeq" %in% assayNames) {
-        source_file <- getFileNames(cancer, runDate, CNA_Seq = TRUE)
-        genome_build <- gsub("(^.+)_(hg[0-9]{2})_(.+$)", "\\2", x = source_file,
-                             ignore.case = TRUE)
-        genome(dataFull$CNASeq) <- genome_build
-        source_file <- as.list(source_file)
-        names(source_file) <- "source_file"
-        metadata(dataFull$CNASeq) <- c(metadata(dataFull$CNASeq), source_file)
-    }
-
-    if("CNACGH" %in% assayNames) {
-        source_file <- getFileNames(cancer, runDate, CNA_CGH = TRUE)
-        genome_build <- gsub("(^.+)_(hg[0-9]{2})_(.+$)", "\\2", x = source_file,
-                             ignore.case = TRUE)
-        genome(dataFull$CNACGH) <- genome_build
-        source_file <- as.list(source_file)
-        names(source_file) <- "source_file"
-        metadata(dataFull$CNACGH) <- c(metadata(dataFull$CNACGH), source_file)
-    }
-
-    ExpList <- ExperimentList(dataFull)
-    NewElist <- TCGAcleanExpList(ExpList, clinicalData)
-    NewMap <- generateMap(NewElist, clinicalData, TCGAbarcode)
-    MAEO <- MultiAssayExperiment(NewElist, clinicalData, NewMap)
-
-    MAEOmeta <- c(cancer, runDate, analyzeDate, sessionInfo())
-    names(MAEOmeta)[1:3] <- c("cohort_name", "running_date", "analysis_data")
-    metadata(MAEO) <- c(metadata(MAEO), MAEOmeta)
-
-    saveRDS(MAEO, file = file.path(dataDirectory,
-                                   paste0(cancerCode, "MAEO.rds")),
-            compress = "bzip2")
-
-    # add lines to csv file for unit tests
-    cohort_name <- rep(cancerCode, length(experiments(MAEO)))
-    experiment_name <- names(MAEO)
-    experiment_class <- vapply(experiments(MAEO), class, character(1L))
-    feature_number <- vapply(experiments(MAEO), function(exp) dim(exp)[[1L]],
-                             integer(1L))
-    sample_number <- vapply(experiments(MAEO), function(exp) dim(exp)[[2L]],
-                            integer(1L))
-    MAEOinfo <- data.frame(cbind(cohort_name, experiment_name, experiment_class,
-                                 feature_number, sample_number))
-    write.table(MAEOinfo, file = "MAEOinfo.csv", sep = ",", append = TRUE,
-                row.names = FALSE, col.names = FALSE)
-  }
-}
-
-# call newMAEO function
-newMAEO(TCGAcodes, runDate, analyzeDate, dataDirectory)
+# call buildMultiAssayExperiment function
+buildMultiAssayExperiments(TCGAcodes, runDate, analyzeDate, dataDirectory)
 
