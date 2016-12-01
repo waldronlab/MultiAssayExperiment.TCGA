9d4707361a8580c1205c6ec6bd467b5f4a31bd1f
LiNk-NY <mramos09@gmail.com>
2016-12-01 12:17:49 -0500

    update package installation

--- R/installLibraries.R
+++ R/installLibraries.R
@@ -1,9 +1,22 @@
-install.packages("devtools", repos = "http://cran.r-project.org")
+## Script for installing necessary packages
+## Use Bioconductor devel and R devel versions
+stopifnot(R.Version()$major == 3 && R.Version()$minor >=4)
 source("https://bioconductor.org/biocLite.R")
 useDevel()
-biocLite()
 BiocInstaller::biocLite("vjcitn/MultiAssayExperiment")
 BiocInstaller::biocLite("LiNk-NY/RTCGAToolbox")
 BiocInstaller::biocLite("waldronlab/BiocInterfaces")
 BiocInstaller::biocLite("karthik/rdrop2")
-install.packages("readr", repos = "http://cran.r-project.org")
+
+install_packages <- function(packageVector) {
+    invisible(lapply(packageVector, function(package) {
+        packageAvail <- do.call(require, args = list(package))
+        if (!packageAvail)
+            do.call(install.packages,
+                    args = list(package,
+                                repos = BiocInstaller::biocinstallRepos()))
+    }))
+}
+
+packs <- c("devtools", "readxl", "readr", "dplyr")
+install_packages(packs)
