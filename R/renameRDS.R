9f836a986f3dadd026bd2f2c207031ea292e0c16
LiNk-NY <mramos09@gmail.com>
2016-12-06 11:18:25 -0500

    add script for renaming rds files

---
+++ R/renameRDS.R
@@ -0,0 +1,12 @@
+## All saved datasets should include capital letter disease codes
+## To keep all scripts consistent
+## e.g., OV, COAD, etc.
+
+dataFiles <- list.files("../data/", pattern = "rds$", full.names = TRUE)
+codeNames <- unlist(lapply(strsplit(basename(dataFiles), "\\."),
+		function(x) { x[[1]] }))
+upperNames <- paste0(toupper(codeNames), ".rds")
+newFiles <- file.path("../data/", upperNames)
+stopifnot(identical(length(dataFiles), length(newFiles)))
+file.rename(dataFiles, newFiles)
+
