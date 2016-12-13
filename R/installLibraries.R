0260e763236c4ed0ff6e80d94ee6c756c13b8df1
LiNk-NY <mramos09@gmail.com>
2016-12-13 18:54:41 -0500

    comment packages installed once

--- R/installLibraries.R
+++ R/installLibraries.R
@@ -2,11 +2,11 @@
 ## Use Bioconductor devel and R devel versions
 
 stopifnot(R.Version()$major == 3 && R.Version()$minor >= 4)
-source("https://bioconductor.org/biocLite.R")
+# source("https://bioconductor.org/biocLite.R")
 # useDevel()
-BiocInstaller::biocLite("devtools")
-BiocInstaller::biocLite("LiNk-NY/RTCGAToolbox")
-BiocInstaller::biocLite("waldronlab/BiocInterfaces")
+# BiocInstaller::biocLite("devtools")
+# BiocInstaller::biocLite("LiNk-NY/RTCGAToolbox")
+# BiocInstaller::biocLite("waldronlab/BiocInterfaces")
 # BiocInstaller::biocLite("karthik/rdrop2")
 
 install_packages <- function(packageVector) {
