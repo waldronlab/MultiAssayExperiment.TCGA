6627d96af7c15235fab7e5d321e0634676bb3e85
LiNk-NY <mramos09@gmail.com>
2016-12-21 12:32:49 -0500

    separate install from load scripts

--- R/installLibraries.R
+++ R/installLibraries.R
@@ -1,25 +1,17 @@
+## --Interactive--
 ## Script for installing necessary packages
 ## Use Bioconductor devel and R devel versions
 
 stopifnot(R.Version()$major == 3 && R.Version()$minor >= 4)
-# source("https://bioconductor.org/biocLite.R")
-# useDevel()
-# BiocInstaller::biocLite("devtools")
-# BiocInstaller::biocLite("LiNk-NY/RTCGAToolbox")
-# BiocInstaller::biocLite("waldronlab/BiocInterfaces")
-# BiocInstaller::biocLite("karthik/rdrop2")
 
-install_packages <- function(packageVector) {
-    invisible(lapply(packageVector, function(package) {
-        packageAvail <- do.call(require, args = list(package))
-        if (!packageAvail)
-            do.call(install.packages,
-                    args = list(package,
-                                repos = BiocInstaller::biocinstallRepos()))
-    }))
-}
+source("https://bioconductor.org/biocLite.R")
 
-packs <- c("readxl", "readr", "dplyr", "AnnotationHubData",
-    "MultiAssayExperiment", "RTCGAToolbox", "BiocInterfaces")
-install_packages(packs)
+useDevel()
+
+library(BiocInstaller)
+biocLite(c("devtools", "readxl", "readr", "dplyr",
+		"AnnotationHubData", "MultiAssayExperiment"))
+biocLite("LiNk-NY/RTCGAToolbox")
+biocLite("waldronlab/BiocInterfaces")
+biocLite("karthik/rdrop2")
 
---
+++ R/loadLibraries.R
@@ -0,0 +1,19 @@
+## Script for loading installed packages
+## See installLibraries.R for necessary packages
+stopifnot(R.Version()$major == 3 && R.Version()$minor >= 4)
+
+install_packages <- function(packageVector) {
+    invisible(lapply(packageVector, function(package) {
+        packageAvail <- do.call(require, args = list(package))
+        if (!packageAvail)
+            do.call(install.packages,
+                    args = list(package,
+                                repos = BiocInstaller::biocinstallRepos()))
+    }))
+}
+
+packs <- c("readxl", "readr", "dplyr", "AnnotationHubData",
+    "MultiAssayExperiment", "RTCGAToolbox", "BiocInterfaces")
+
+install_packages(packs)
+
