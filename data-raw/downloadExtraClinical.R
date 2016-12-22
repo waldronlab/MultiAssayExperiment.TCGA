8184c01b7faf64a866a4df04fedf21cbf1a604be
LiNk-NY <mramos09@gmail.com>
2016-12-21 20:58:51 -0500

    use fileName in write_csv

--- data-raw/downloadExtraClinical.R
+++ data-raw/downloadExtraClinical.R
@@ -9,9 +9,7 @@ writeClinicalData <- function(diseaseCode, force=FALSE) {
     fileName <- file.path(enhancedPath, paste0(diseaseCode, ".csv"))
     if (!file.exists(fileName) || force) {
     dataset <- .mergeClinicalData(diseaseCode)
-    readr::write_csv(dataset,
-              path = file.path(enhancedPath,
-                               paste0(diseaseCode, ".csv")))
+    readr::write_csv(dataset, path = fileName)
     message(dataset, " with extra columns created")
     rm(dataset)
     }
