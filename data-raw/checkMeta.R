8411886547f50b5c11f021bfe2af348d6c40a833
LiNk-NY <mramos09@gmail.com>
2017-01-05 16:13:38 -0500

    rename checks for clinical metadata

---
+++ data-raw/checkMeta.R
@@ -0,0 +1,33 @@
+## Code to check file metadata
+## (including dimensions and proportion of cells missing)
+source("R/getDiseaseCodes.R")
+source("R/dataDirectories.R")
+TCGAcodes <- getDiseaseCodes()
+dirList <- dataDirectories()
+
+checkMeta <- function(diseaseCode) {
+    listDirs <- dirList[c("basicClinical", "enhancedClinical",
+                          "mergedClinical")]
+    resolvePaths <- lapply(listDirs, function(directory) {
+                               hits <- list.files(directory, full.names = TRUE,
+                                                  pattern = paste0("^", diseaseCode))
+                               hits[!grepl("dropped.rds$", basename(hits))]
+                          })
+    locations <- unlist(resolvePaths)
+    names(locations) <- c("basic", "enhanced", "merged", "reduced")
+    metadata <- lapply(locations, function(fileLocation) {
+                           dataFile <- readr::read_csv(fileLocation)
+                           cellMissing <- sum(rapply(dataFile, is.na))
+                           datDims <- dim(dataFile)
+                           totalCell <- datDims[[1L]]*datDims[[2L]]
+                           cbind.data.frame(rows = datDims[[1L]],
+                                            columns = datDims[[2L]],
+                                            prop.missing = cellMissing/totalCell)
+                          })
+    cbind.data.frame(file.location = locations, do.call(rbind, metadata))
+}
+
+mdata <- lapply(TCGAcodes, checkMeta)
+
+saveRDS(mdata, "metadataChecks.Rds")
+
