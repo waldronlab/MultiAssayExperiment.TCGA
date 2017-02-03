8c46a28f5073769347f27a371ba940b133d764e7
LiNk-NY <mramos09@gmail.com>
2017-02-02 20:21:06 -0500

    add scripts for saving and documenting data bits

---
+++ R/saveData.R
@@ -0,0 +1,19 @@
+saveData <- function(dataList, cancer, directory = "data/bits") {
+    if (!dir.exists(directory))
+        dir.create(directory, recursive = TRUE)
+    filePrefix <- paste0(toupper(cancer), "_")
+    filetype <- ".rda"
+    dataNames <- names(dataList)
+    stopifnot(!is.null(dataNames))
+    objnames <- paste0(filePrefix, dataNames)
+    fnames <- file.path(directory, paste0(objnames, ".rda"))
+    for (i in seq_along(dataList)) {
+        message(paste0("Writing: ", fnames[i]))
+        objname <- objnames[i]
+        assign(x = objname, value = dataList[[i]])
+        save(list = objname,
+             file = fnames[i],
+             compress = "bzip2")
+        updateMetadata(dataList[i], cancer)
+    }
+}
