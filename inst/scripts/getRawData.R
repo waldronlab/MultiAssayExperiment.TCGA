b2eea273ad48e6c0c8ffc5c49c3bf48363a86e51
LiNk-NY <mramos09@gmail.com>
2017-02-22 12:03:25 -0500

    Add folder check

--- inst/scripts/getRawData.R
+++ inst/scripts/getRawData.R
@@ -5,12 +5,15 @@ library(readr)
 library(RTCGAToolbox)
 library(TCGAutils)
 
+setwd("../")
+stopifnot(identical(basename(getwd()), "MultiAssayExperiment-TCGA"))
 source("R/getDiseaseCodes.R")
 source("R/saveRTCGAdata.R")
 
 TCGAcodes <- getDiseaseCodes()
 runDate <- "20160128"
 analyzeDate <- "20160128"
-directory <- "data"
+directory <- "data/raw"
 
 lapply(TCGAcodes, saveRTCGAdata, runDate, analyzeDate, directory)
+
