043d5ee8d115eb4ed8b15ac2436f58724b578303
LiNk-NY <mramos09@gmail.com>
2017-05-09 19:47:24 -0400

    set dims for GRangesList in MAEOinfo.csv

--- R/updateInfo.R
+++ R/updateInfo.R
@@ -4,8 +4,13 @@ updateInfo <- function(dataElement, cancerCode) {
     assayName <- names(dataElement)
     stopifnot(S4Vectors::isSingleString(assayName))
     className <- class(dataObject)
+    if (is(dataObject, "GRangesList")) {
+        numberRow <- NA_integer_
+        numberCol <- length(dataObject)
+    } else {
     numberRow <- dim(dataObject)[[1L]]
     numberCol <- dim(dataObject)[[2L]]
+    }
     MAEOinfo <- cbind.data.frame(cancerCode, assayName, className, numberRow,
                                  numberCol)
     write.table(MAEOinfo, file = "MAEOinfo.csv", sep = ",",
