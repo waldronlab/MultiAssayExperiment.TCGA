3660865c6e4aa46c4c2a2c1e18d0d817b828b170
LiNk-NY <mramos09@gmail.com>
2017-08-09 12:52:04 -0400

    remove verbose message

--- R/saveNupload.R
+++ R/saveNupload.R
@@ -8,12 +8,9 @@ saveNupload <- function(dataList, cancer, directory = "data/bits") {
     objnames <- dataNames
     fnames <- file.path(cancerSubdir, paste0(objnames, ".rda"))
     for (i in seq_along(dataList)) {
-        message(paste0("Writing: ", fnames[i]))
         objname <- objnames[i]
         assign(x = objname, value = dataList[[i]])
-        save(list = objname,
-             file = fnames[i],
-             compress = "bzip2")
+        save(list = objname, file = fnames[i], compress = "bzip2")
         AnnotationHubData:::upload_to_S3(file = fnames[i],
                                          remotename = basename(fnames[i]),
                                          bucket =
