074c124ea17167e24017cddd5ed11e9d7ed48616
LiNk-NY <mramos09@gmail.com>
2017-09-29 14:20:08 -0400

    convert to shell script, rm(obj)

---
+++ exec/updateObj.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+time /home/$USER/src/svn/r-devel/R/bin/Rscript ../inst/scripts/updateRTCGA.R --verbose
+
--- inst/scripts/updateRTCGA.R
+++ inst/scripts/updateRTCGA.R
@@ -1,23 +1,46 @@
-# Update RTCGAToolbox objects
+# Ensure location
+message(getwd())
+setwd("..")
+stopifnot(identical(getwd(),
+    paste0("/home/", Sys.getenv("USER"),
+        "/Documents/github/MultiAssayExperiment-TCGA")))
 
-dataFiles <- list.files("data/raw", pattern = "*\\.rds$", full.names = TRUE,
-    recursive = TRUE)
+# Update RTCGAToolbox objects
+library(TCGAutils)
+data(diseaseCodes)
+
+diseaseCodes <- diseaseCodes[[1L]]
+
+# If subset needs to be run, replace cancer code with last unsuccessful attempt
+diseaseCodes <- diseaseCodes[which(diseaseCodes == "LUAD"):length(diseaseCodes)]
+
+foldersCheck <- file.path("data/raw", diseaseCodes)
+message("checking in folders:", sprintf("%s, ", foldersCheck))
+
+dataFiles <- list.files(foldersCheck,
+    pattern = "*\\.rds$", full.names = TRUE, recursive = FALSE)
 
 dataNames <- vapply(strsplit(basename(dataFiles), "\\."), `[`,
     character(1L), 1L)
 
+names(dataFiles) <- dataNames
+
 folderName <- vapply(strsplit(dataNames, "_"), `[`, character(1L),
     1L)
 
-lapply(folderName, function(x) dir.create(file.path("data", "rawold", x)))
-
-names(dataFiles) <- dataNames
+invisible(lapply(folderName, function(x) {
+    folderPath <- file.path("data", "rawold", x)
+    if(!dir.exists(folderPath))
+    dir.create(file.path("data", "rawold", x))
+}))
 
 lapply(seq_along(dataFiles), function(i, file) {
+    message("Updating ", names(file[i]))
     dataObj <- RTCGAToolbox::updateObject(readRDS(file[i]))
-    readr::write_rds(x = assign(names(file[i]), dataObj),
+    readr::write_rds(x = dataObj,
         path = file.path("data/rawold", folderName[i],
             paste0(names(file[i]), ".rds")),
         compress = "bz2")
+    rm(dataObj)
 }, file = dataFiles)
 
