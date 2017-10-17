816f070029ca266b3163a33865675aec898da1ff
LiNk-NY <mramos09@gmail.com>
2017-10-17 15:46:44 -0400

    add force arg, condition on file avail

--- data-raw/cleanMerged.R
+++ data-raw/cleanMerged.R
@@ -4,34 +4,29 @@ source("data-raw/helpers.R")
 
 TCGAcodes <- getDiseaseCodes()
 
-cleanMerged <- function(diseaseCode, runDate = "20160128") {
+cleanMerged <- function(diseaseCode, runDate = "20160128", force = FALSE) {
     mergedLoc <- dataDirectories()[["mergedClinical"]]
     fileName <- file.path(mergedLoc,
-                          paste(runDate,
-                                paste0(diseaseCode, "_reduced.csv"),
-                                sep = "-")
-    )
-    if (!file.exists(fileName)) {
-        fullClinical <-
-            readr::read_csv(
-                file.path(mergedLoc, paste(runDate,
-                                           paste0(diseaseCode, "_merged.csv"),
-                                           sep = "-"))
-            )
+        paste(runDate, paste0(diseaseCode, "_reduced.csv"), sep = "-"))
+
+    droppedFile <- file.path(mergedLoc,
+        paste(runDate, paste0(diseaseCode, "_dropped.rds"), sep = "-"))
+
+    if (!file.exists(fileName) || force) {
+        fullClinical <- readr::read_csv(
+            file.path(mergedLoc, paste(runDate, paste0(diseaseCode,
+                "_merged.csv"), sep = "-")))
+
         NACols <- .findNAColumns(fullClinical)
         droppedCols <- names(fullClinical)[NACols]
-        saveRDS(droppedCols,
-                file = file.path(
-                    mergedLoc, paste(runDate,
-                                     paste0(diseaseCode, "_dropped.rds"),
-                                     sep = "-")
-                )
-        )
-        write_csv(fullClinical[, !NACols], path = fileName)
-        message(diseaseCode, " completed!")
-        return()
+
+    if (!file.exists(droppedFile) || force) {
+        saveRDS(droppedCols, file = file.path(mergedLoc,
+            paste(runDate, paste0(diseaseCode, "_dropped.rds"), sep = "-")))
     }
-    message(diseaseCode, "_reduced.csv already available!")
+        write_csv(fullClinical[, !NACols], path = fileName)
+        message(diseaseCode, " saved at ", fileName)
+    } else message(diseaseCode, "_reduced.csv already available!")
 }
 
 BiocParallel::bplapply(TCGAcodes, cleanMerged)
