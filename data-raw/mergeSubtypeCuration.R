5a688811a6fbbd0e1165fc8ba1a51e0f6b98bb93
LiNk-NY <mramos09@gmail.com>
2017-10-17 15:47:34 -0400

    add force arg

--- data-raw/mergeSubtypeCuration.R
+++ data-raw/mergeSubtypeCuration.R
@@ -4,15 +4,18 @@
 load("data/curationAvailable.rda")
 
 ## Save final merged datasets
-writeMergedClinical <- function(diseaseCode, runDate = "20160128", curationAvailable) {
+writeMergedClinical <- function(diseaseCode, runDate = "20160128",
+        curationAvailable, force = FALSE) {
     dirList <- dataDirectories()
     mergedClinical <- dirList[["mergedClinical"]]
     fileName <- file.path(mergedClinical, paste(runDate,
                           paste0(diseaseCode, "_merged.csv"), sep = "-"))
-    if (!file.exists(fileName)) {
+    if (!file.exists(fileName) || force) {
         mergedData <- .mergeSubtypeClinical(diseaseCode, runDate = runDate,
                                             curationAvailable)
         write_csv(x = mergedData, path = fileName)
+        message(diseaseCode, " curation merged to clincial data!\n",
+        "See: ", fileName)
     }
 }
 
