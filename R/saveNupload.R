90533ec9d46bea842dc63c2ff615168d0ed0501a
LiNk-NY <mramos09@gmail.com>
2018-09-30 23:57:02 -0400

    update pipeline code to save methyl files

--- R/saveNupload.R
+++ R/saveNupload.R
@@ -1,20 +1,35 @@
+.saveMethylHDF5 <- function(objname, filepaths) {
+    folder <- unique(dirname(filepaths))
+    stopifnot(length(folder) == 1L)
+    h5exts <- c("assays.h5", "se.rds")
+    HDF5Array::saveHDF5SummarizedExperiment(
+        x = get(objname), dir = folder)
+    file.rename(file.path(folder, h5exts), filepaths)
+    filepaths
+}
+
 saveNupload <- function(dataList, cancer, directory = "data/bits",
     upload = TRUE) {
     cancerSubdir <- file.path(directory, cancer)
     if (!dir.exists(cancerSubdir))
         dir.create(cancerSubdir, recursive = TRUE)
     filetype <- ".rda"
+    methylext <- c(".h5", ".rds")
     dataNames <- names(dataList)
     stopifnot(!is.null(dataNames))
-    objnames <- dataNames
-    fnames <- file.path(cancerSubdir, paste0(objnames, ".rda"))
-    for (i in seq_along(dataList)) {
-        objname <- objnames[i]
-        assign(x = objname, value = dataList[[i]])
-        save(list = objname, file = fnames[i], compress = "bzip2")
-    if (upload)
-        AnnotationHubData:::upload_to_S3(file = fnames[i],
-            remotename = basename(fnames[i]),
-            bucket = "experimenthub/curatedTCGAData")
+    for (objname in dataNames) {
+        assign(x = objname, value = dataList[[objname]])
+        if (grepl("Methyl", objname, ignore.case = TRUE)) {
+            mfolder <- file.path(cancerSubdir, objname)
+            filenames <- file.path(mfolder, paste0(objname, methylext))
+            fnames <- .saveMethylHDF5(objname, filenames)
+        } else {
+            fnames <- file.path(cancerSubdir, paste0(objname, filetype))
+            save(list = objname, file = fnames, compress = "bzip2")
+        }
+        if (upload)
+            AnnotationHubData:::upload_to_S3(file = fnames,
+                remotename = basename(fnames),
+                bucket = "experimenthub/curatedTCGAData")
     }
 }
