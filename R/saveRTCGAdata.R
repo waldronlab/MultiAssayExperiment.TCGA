52116e610f08269a0b43fb9e2aad54a51e192308
LiNk-NY <mramos09@gmail.com>
2018-11-30 17:54:14 -0500

    add defaults and fix gistic arguments

--- R/saveRTCGAdata.R
+++ R/saveRTCGAdata.R
@@ -1,7 +1,8 @@
-saveRTCGAdata <- function(runDate, diseaseCode, dataType = c("RNASeqGene",
-    "miRNASeqGene", "RNASeq2GeneNorm", "CNASNP", "CNVSNP", "CNASeq",
-    "CNACGH", "Methylation", "Mutation", "mRNAArray", "miRNAArray",
-    "RPPAArray", "GISTIC"), analyzeDate, directory, force = FALSE) {
+saveRTCGAdata <- function(runDate = "20160128", diseaseCode,
+    dataType = c("RNASeqGene", "miRNASeqGene", "RNASeq2GeneNorm", "CNASNP",
+    "CNVSNP", "CNASeq", "CNACGH", "Methylation", "Mutation", "mRNAArray",
+    "miRNAArray", "RPPAArray", "GISTIC"), analyzeDate = "20160128", directory,
+    force = FALSE) {
     if (!dir.exists(file.path(directory, diseaseCode)))
         dir.create(file.path(directory, diseaseCode), recursive = TRUE)
     choices <- match.arg(dataType, c("RNASeqGene", "miRNASeqGene",
@@ -15,14 +16,14 @@ saveRTCGAdata <- function(runDate, diseaseCode, dataType = c("RNASeqGene",
             gistic <- grepl("^GIST", dataType, ignore.case = TRUE)
             if (gistic) {
                 dateType <- "gistic2Date"
-                args <- list(diseaseCode, analyzeDate)
-                names(args) <- c("dataset", dateType)
+                args <- list(diseaseCode, analyzeDate, TRUE)
+                names(args) <- c("dataset", dateType, dataType)
             } else {
                 dateType <- "runDate"
                 args <- list(diseaseCode, runDate, TRUE)
                 names(args) <- c("dataset", dateType, dataType)
             }
-                dataPiece <- do.call(getFirehoseData, args = c(
+                dataPiece <- do.call(RTCGAToolbox::getFirehoseData, args = c(
                     list(clinical = FALSE, destdir = "./tmp",
                          fileSizeLimit = 500000), args))
                 saveRDS(dataPiece, file = rdsPath, compress = "bzip2")
