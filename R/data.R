abf9309a22b366fe6e5bdb6df980f98b202b1851
LiNk-NY <mramos09@gmail.com>
2019-04-06 23:34:26 -0400

    updates

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @name curationAvailable
+#' @title TCGA disease codes that have been curated
+#' @docType data
+#' @description
+#' A character vector indication what TCGA cancer codes have associated
+#' curation data obtained from publications.
+#' @keywords data
+#' @author Marcel Ramos \email{marcel.ramos@roswellpark.org}
+#' @source https://github.com/waldronlab/multiassayexperiment-tcga
+#'
+#' @examples
+#' data('curationAvailable')
+#'
+"curationAvailable"
--- R/dataDirectories.R
+++ R/dataDirectories.R
@@ -11,28 +11,29 @@
 dataDirectories <- function(create=FALSE) {
     extData <- "inst/extdata/"
     clinicalData <- file.path(extData, "Clinical")
-    directories <- list(
-                        extData = extData,
-                        clinicalData = clinicalData,
-                        curatedSubtypes =
-                            file.path(extData, "curatedSubtypes"),
-                        curatedMaps = file.path(extData, "curatedSubtypes",
-                                                "curatedMaps"),
-                        subtypePath = file.path(extData, "allsubtypes"),
-                        clinicalCurationPath =
-                            file.path(extData, "TCGA_Clinical_Curation"),
-                        rawClinical = file.path(clinicalData, "raw"),
-                        basicClinical = file.path(clinicalData, "basic"),
-                        enhancedClinical = file.path(clinicalData, "enhanced"),
-                        mergedClinical = file.path(clinicalData, "merged")
-                        )
+    directories <- IRanges::CharacterList(
+        extData = extData,
+        clinicalData = clinicalData,
+        curatedSubtypes =
+            file.path(extData, "curatedSubtypes"),
+        curatedMaps = file.path(extData, "curatedSubtypes",
+                                "curatedMaps"),
+        subtypePath = file.path(extData, "allsubtypes"),
+        clinicalCurationPath =
+            file.path(extData, "TCGA_Clinical_Curation"),
+        rawClinical = file.path(clinicalData, "raw"),
+        basicClinical = file.path(clinicalData, "basic"),
+        enhancedClinical = file.path(clinicalData, "enhanced"),
+        mergedClinical = file.path(clinicalData, "merged")
+    )
     if (create) {
     message("Creating 'inst' directory in ", getwd())
         invisible(lapply(directories,
-                         function(folder) {
-                             if (!file.exists(folder))
-                                 dir.create(folder, recursive = TRUE)
-                         }))
+            function(folder) {
+                if (!file.exists(folder))
+                    dir.create(folder, recursive = TRUE)
+            }
+        ))
     }
     return(directories)
 }
