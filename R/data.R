d772ffb802f519e01cfb825954799c30e235b014
LiNK-NY <mramos09@gmail.com>
2023-03-27 14:49:35 -0400

    export and document curationAvailable

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' @docType data
+#'
+#' @title TCGA disease codes that have been curated
+#'
+#' @description A character vector indication what TCGA cancer codes have
+#'   associated curation data obtained from publications.
+#'
+#' @format An named `character` vector of length 19
+#'
+#' @source https://github.com/waldronlab/MultiAssayExperiment.TCGA
+#' @usage data("curationAvailable")
+#'
+#' @author Marcel Ramos <marcel.ramos@roswellpark.org>
+#' @keywords data
+#'
+#' @examples
+#'
+#' data("curationAvailable")
+#'
+#' @export
+"curationAvailable"
